bffa8e29c48aceb8a46f1053993b7a735ce4c376
Kith_ <131180636+Vampsoutside@users.noreply.github.com>
2026-08-02 20:01:10 +0530

    Time: 300 ms (16.68%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,12 @@
+# Kith_
+select 
+start.machine_id,
+round(avg(end.timestamp - start.timestamp),3) as processing_time
+
+from Activity start
+join Activity end
+on start.machine_id = end.machine_id 
+and start.activity_type = 'start'
+and end.activity_type = 'end'
+group by machine_id;
+
