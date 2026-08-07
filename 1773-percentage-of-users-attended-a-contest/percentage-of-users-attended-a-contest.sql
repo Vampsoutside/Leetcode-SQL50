30529bfe0844d98be33025761d1fdaafc16a0c71
Kith_ <131180636+Vampsoutside@users.noreply.github.com>
2026-08-07 19:06:59 +0530

    Time: 1480 ms (7.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,10 @@
+# Kith_
+select distinct(contest_id),
+round(
+    count(distinct user_id) * 100 / (select count(*)
+                                 from users),2) as percentage
+
+from Register
+group by contest_id
+order by percentage desc,
+contest_id asc
