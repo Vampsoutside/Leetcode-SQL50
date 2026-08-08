c67acbcc72334bbf9d6f87d343abc9b09c9c8918
Kith_ <131180636+Vampsoutside@users.noreply.github.com>
2026-08-08 17:32:36 +0530

    Time: 796 ms (11.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,11 @@
+# Kith_
+select date_format(trans_date, '%Y-%m') as month,
+country,
+count(*) as trans_count,
+sum(state = 'approved') as approved_count,
+sum(amount) as trans_total_amount,
+sum(amount * (state = 'approved')) as approved_total_amount
+
+from Transactions
+group by month,
+country;
